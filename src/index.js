90beee6c58d43b766e9e5abe132f7a1386d5f570
unknown <semohammadkhani@gmail.com>
2022-07-16 09:48:35 -0400

    season configuration

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReacDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends React.Component {
+  state = {
+    lat: null,
+    errorMessage: "",
+  };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          lat: position.coords.latitude,
+        });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+  render() {
+    if (this.state.lat && !this.state.errorMessage) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    if (!this.state.lat && this.state.errorMessage) {
+      return <div>Error:{this.state.errorMessage}</div>;
+    }
+    return <div>Loading!</div>;
+  }
+}
+ReacDOM.render(<App />, document.querySelector("#root"));
